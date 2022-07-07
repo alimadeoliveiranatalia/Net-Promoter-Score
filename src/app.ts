85b2836d0c7460b592fac1f8a7439e1dd62ac46a
Natalia <alimadeoliveiranatalia@gmail.com>
2022-07-07 14:39:21 -0300

    feat: add swagger api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors"
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../swagger.json";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
